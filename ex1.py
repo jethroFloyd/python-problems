0f4a988832a3b749d8167dd357a9639fa576fa89
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-06-09 05:02:50 +0100

    initial practice

---
+++ ex1.py
@@ -0,0 +1,4 @@
+print "Hello World!"
+print 'Yay it works!'
+print "I'd much rather you 'not'self."
+print 'I said "do not" touch this.'
---
+++ ex3.py
@@ -0,0 +1,6 @@
+#trying to manipulate numbers around
+
+print "Find 3 + 2 ", 3 + 2
+print "Find the boolean if 3 > -2?", 3 > -2
+print "Integer 3/2?", 3/2
+print "To convert to float, use 3.0/2", 3.0/2 
