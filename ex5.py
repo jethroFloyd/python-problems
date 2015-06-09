2805caa8e6eabf5f887f9a6128024bf6a54291fc
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-06-09 05:31:37 +0100

    finished with printing stuff

--- ex5.py
+++ ex5.py
@@ -11,7 +11,12 @@ print a1 + a3,
 print a1 + a3,
 print a2 + a4
 
-print """lots of lines
+print '''
+lots of lines
 hahaha
 yoyo
-hahaha"""
+hahaha
+'''
+
+print "yoyo \a" #ascii bell
+print "yoyo \b man" #ascii backspace
