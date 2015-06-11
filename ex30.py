dca09ef65910cd81079986cdbff5b640d0c9749e
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-06-11 05:14:43 +0100

    small bear game

---
+++ ex30.py
@@ -0,0 +1,14 @@
+## for loops and lists
+
+the_count = [1,2,3,4,5]
+
+for number in the_count:
+	print number
+
+empty_list = []
+
+for i in range(0,6):
+	empty_list.append(2*i)
+
+for number in empty_list:
+	print number
