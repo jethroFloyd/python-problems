3cda242b536d1c42f548f8a7f1feb6865e149d95
Ritobroto Maitra <ritobrotomaitra@gmail.com>
2015-06-11 04:56:58 +0100

    conditionals

---
+++ ex29.py
@@ -0,0 +1,16 @@
+#starting with branching and if-control
+
+people = 20
+dogs = int(raw_input("Number of Dogs?"))
+
+if people > dogs:
+	print "People > Dogs"
+else:
+	print "blah"
+
+
+people = 25
+dogs = 90
+
+if (0 < dogs < 100) :
+	print "yoyo"
